a6795a8c5bd73887ab92f84c51601064375db3b7
Mingeon Kim <issac4892@gmail.com>
2024-12-01 00:34:09 +0900

    ✨ error and gitignore

---
+++ src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  <title>{$page.status}</title>
+</svelte:head>
+
+<div class="container">
+  <div class="emphasis">{$page.status}</div>
+  <div class="soon">{$page.error}</div>
+</div>
+
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+    justify-content: center;
+    align-items: center;
+
+    gap: 30px;
+  }
+
+  .soon {
+    font-size: 12px;
+  }
+
+  .emphasis {
+    font-weight: 700;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,43 +7,6 @@
 </div>
 
 <style>
-  @font-face {
-    font-family: 'LINE Seed KR';
-    font-weight: 400;
-    src: url('/fonts/LINESeedKR-Rg.woff2') format('woff2');
-  }
-
-  @font-face {
-    font-family: 'LINE Seed KR';
-    font-weight: 100;
-    src: url('/fonts/LINESeedKR-Th.woff2') format('woff2');
-  }
-
-  @font-face {
-    font-family: 'LINE Seed KR';
-    font-weight: 700;
-    src: url('/fonts/LINESeedKR-Bd.woff2') format('woff2');
-  }
-
-  :root {
-    font-family:
-      'LINE Seed KR',
-      system-ui,
-      -apple-system,
-      BlinkMacSystemFont,
-      'Segoe UI',
-      Roboto,
-      Oxygen,
-      Ubuntu,
-      Cantarell,
-      'Open Sans',
-      'Helvetica Neue',
-      sans-serif;
-
-    width: 100%;
-    height: 100%;
-  }
-
   .layout {
     width: 100%;
     height: 100%;
